3600303acb594f346a1eba5a30a229e8cb8c08cd
Luis Guitron <legl_1995@hotmail.com>
2019-07-11 22:28:19 -0500

    Tested softmax agent

--- Connect4/main.py
+++ Connect4/main.py
@@ -20,9 +20,9 @@ egreedy_dqn_agent       = DQNAgent(board_size, "models/egreedy_2M", model, model
 
 
 # Softmax Agent                                    |
-#temperature_vals        = [0.200, 0.050, 0.025, 0.020, 0.016, 0.013]
-#temperature_decay       = [50000, 100000, 500000, 1000000, 1500000]
-#softmax_dqn_agent       = DQNAgent(board_size, "models/dqn_model_6_7", model, model_name, AgentType.Softmax, temperature_vals, temperature_decay)
+temperature_vals        = [0.200, 0.050, 0.025, 0.020, 0.016, 0.013]
+temperature_decay       = [50000, 100000, 500000, 1000000, 1500000]
+softmax_dqn_agent       = DQNAgent(board_size, "models/softmax_2M", model, model_name, AgentType.Softmax, temperature_vals, temperature_decay)
 
 
 # Random Agent
@@ -36,16 +36,14 @@ train_episodes          = 1000
 test_episodes           = 1
 test_train_epochs       = 2000
 display_stats_frequency = 1000              # Display stats after this amount of games
-
-
 train_DQN_agent(softmax_dqn_agent, train_episodes, test_episodes, test_train_epochs, board_size, connect_to_win, display_stats_frequency)
 plot_results(softmax_dqn_agent)
 '''
 
-# Play against RL agent
-rendered_games(egreedy_dqn_agent, human_agent, board_size, connect_to_win)
-
 # Test Match between agents
-#sim_games(dqn_agent, prev_dqn_agent, board_size, connect_to_win, episodes=1000, doTraining=False, is_exploring=True, display_results = True)
-#sim_games(dqn_agent, prev_dqn_agent, board_size, connect_to_win, episodes=2, doTraining=False, is_exploring=False, display_results = True)
-#sim_games(egreedy_dqn_agent, rand_agent, board_size, connect_to_win, episodes=10000, doTraining=False, is_exploring=False, display_results = True)
+sim_games(egreedy_dqn_agent, rand_agent, board_size, connect_to_win, episodes=10000, doTraining=False, is_exploring=False, display_results = True)
+sim_games(softmax_dqn_agent, rand_agent, board_size, connect_to_win, episodes=10000, doTraining=False, is_exploring=False, display_results = True)
+sim_games(softmax_dqn_agent, egreedy_dqn_agent, board_size, connect_to_win, episodes=2, doTraining=False, is_exploring=False, display_results = True)
+
+# Play against RL agent
+# rendered_games(softmax_dqn_agent, human_agent, board_size, connect_to_win)
